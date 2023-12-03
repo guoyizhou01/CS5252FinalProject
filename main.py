cda29bd9e2bc6753d5d2da26b743a173dd1e2915
guoyizhou01 <85047186+guoyizhou01@users.noreply.github.com>
2023-12-03 11:46:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def read_cfg(filename):
 		first_line = True
 		for line in file:
 			if first_line:
-				start = line.strip().replace(" ","")
+				start = line.strip().replace(" ","").replace("~","ε")
 				first_line = False
 				continue
 			# split_line[0] is state, split_line[1] is all possible productions	
-			split_line = line.strip().replace(" ","").split("->")
+			split_line = line.strip().replace(" ","").replace("~","ε").split("->")
 			productions = split_line[1].split("|")
 			for production in productions:
 				single_prod = []
@@ -27,11 +27,11 @@ def read_cfg(filename):
 							print("ERROR: Syntax Error in %s: %s!"%(filename,split_line))
 							exit(1)
 						if production[i+1] == "{":
-							while (i < len(production) and production[i] != "}"):
+							while (i < len(production)-1 and production[i] != "}"):
 								i = i+1
 								state = state + production[i]
-							if i < len(production) and production[i] == "}":
-								state = state + production[i]
+							# if i == len(production) and production[i-1] == "}":
+								# state = state + production[i]
 							if state[-1] != "}":
 								print("ERROR: Syntax Error in %s: %s!"%(filename,split_line))
 								exit(1)
@@ -55,24 +55,68 @@ def read_cfg(filename):
 def is_state(string):
 	return string[0] == "%"
 
-def cfg_to_pda(start,grammar):
+def cfg_to_pda(start,grammar,sort_grammar = False):
 	index = dict()
-	print("δ(q0,ε,ε)=δ(q_{0:}_0,{0:})".format(start))
+	print('Start state: q_START')
+	print("δ(q_START,ε,ε)=δ(q_{0:}_0,{{ACCEPT}})".format(start))
+	state_shift = dict()
+	end_of_state = dict()
+	pre_accept = []
+	grammar_str = []
 	for key in grammar.keys():
 		index[key] = 0
 	for key in grammar.keys():
-		production = grammar[key]
-		for i in range(len(production)):
-			if is_state(production[i]):
-				print("δ(q_{0:}_{1:},ε,ε)=δ(q_{2:}_0,{0:}_{1})".format(key,index[key],production[i]) )
-			elif :
-				print("δ(q_{0:}_{1:},ε,ε)=δ(q_{2:}_0,{0:}_{1})".format(key,index[key],production[i]) )
+		for production in grammar[key]:
+			isStart = True
+			symbolEnd = 'ε'
+			for i in range(len(production)):
+				if is_state(production[i]):
 
+					if isStart:
+						grammar_str.append("δ(q_{0:}_0,ε,ε)=δ(q_{2:}_0,{0:}_{3})".format(key,index[key],production[i],index[key]+1 ))
+						isStart = False
+					else:
+						grammar_str.append("δ(q_{0:}_{1:},ε,ε)=δ(q_{2:}_0,{0:}_{3})".format(key,index[key],production[i],index[key]+1 ))
+					if not (production[i] in state_shift.keys()):
+						state_shift[production[i]] = []
+					state_shift[production[i]].append("{0:}_{1}".format(key,index[key]+1))
+
+				else :
+					if isStart:
+						grammar_str.append("δ(q_{0:}_0,{3:},ε)=δ(q_{0:}_{2:},ε)".format(key,index[key],index[key]+1,production[i]) )
+						isStart = False
+					elif i == len(production) - 1:
+						symbolEnd = production[i]
+						index[key] = index[key]-1
+					else:
+						grammar_str.append("δ(q_{0:}_{1:},{3:},ε)=δ(q_{0:}_{2:},ε)".format(key,index[key],index[key]+1,production[i]) )
+
+				index[key] = index[key]+1
+			if key == start:
+				pre_accept.append((index[key],symbolEnd))
+			if not (key in end_of_state.keys()):
+				end_of_state[key] = []
+			end_of_state[key].append((index[key],symbolEnd))
+	# print(index)
+	for key in state_shift.keys():
+		for end_pair in end_of_state[key]:
+			for return_state in state_shift[key]:
+				grammar_str.append("δ(q_{0:}_{1:},{3:},{2:})=δ(q_{2:},ε)".format(key,end_pair[0],return_state,end_pair[1]))
+	for accept_pair in pre_accept:
+		grammar_str.append("δ(q_{0:}_{1:},{2:},{{ACCEPT}})=δ(q_END,ε)".format(start,accept_pair[0],accept_pair[1]))
+	if sort_grammar:
+		grammar_str.sort()
+	for line in grammar_str:
+		print(line)
+	print('Accept by empty stack only')
 
 
 if __name__ == '__main__':
 	filename = sys.argv[1].strip()
 	(start,grammar) = read_cfg(filename)
-	print(start)
-	print(grammar)
-	cfg_to_pda(start,grammar)
+	sort_result = False
+	if len(sys.argv) > 2 and sys.argv[2] == 'sort':
+		sort_result = True		
+	print('start:',start)
+	print('grammar:',grammar)
+	cfg_to_pda(start,grammar,sort_result)
